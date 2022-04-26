140ceecf587980dfff04aea886cace57e10ae8cd
Alexander Thalhammer <alex@angular-software.at>
2022-04-26 16:44:34 +0200

    Add extra observavle/subscription/async demo

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,35 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 import { Flight } from '../entities/flight';
 import { FlightService } from './flight.service';
+import { Observable, Observer, Subject, Subscription } from 'rxjs';
+import { share, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css']
 })
-export class FlightSearchComponent {
+export class FlightSearchComponent implements OnDestroy {
   from = 'Graz';
   to = 'Hamburg';
+
   flights: Flight[] = [];
+  flights$: Observable<Flight[]> | undefined;
+  flightsSubscription: Subscription | undefined;
+
   selectedFlight: Flight | undefined | null;
 
   message = '';
 
+  private readonly onDestroySubject = new Subject<void>();
+  readonly terminator$ = this.onDestroySubject.asObservable();
+
   constructor(private flightService: FlightService) {}
 
   search(): void {
-    this.flightService.find(this.from, this.to).subscribe({
-      next: (flights) => {
-        this.flights = flights;
-      },
-      error: (errResp) => {
-        console.error('Error loading flights', errResp);
-      },
-      complete: () => {
-        console.warn('complete');
-      }
-    });
+    // 1. my observable
+    this.flights$ = this.flightService.find(this.from, this.to).pipe(share());
+
+    // 2. my observer
+    const flightsObserver: Observer<Flight[]> = {
+      next: (flights) => (this.flights = flights),
+      error: (errResp) => console.error('Error loading flights', errResp),
+      complete: () => console.warn('complete')
+    };
+
+    // 3a. my subscription
+    // this.flightsSubscription?.unsubscribe();
+    // this.flightsSubscription = this.flights$.subscribe(flightsObserver);
+
+    // 3b. takeUntil terminator$ emits
+    this.flights$.pipe(takeUntil(this.terminator$)).subscribe(flightsObserver);
+  }
+
+  ngOnDestroy(): void {
+    // 4a. my unsubscribe
+    // this.flightsSubscription?.unsubscribe();
+
+    // 4b. subject emit thru terminator$
+    this.onDestroySubject.next(void 0);
+    this.onDestroySubject.complete();
   }
 
   select(f: Flight): void {
