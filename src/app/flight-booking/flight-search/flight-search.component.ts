7218494295d9946630ea71be82b11af0bee2b9cc
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-04-20 09:24:49 +0200

    [T-d forms] Reusable component for displaying validation errors

--- src/app/flight-booking/flight-search/flight-search.component.ts
+++ src/app/flight-booking/flight-search/flight-search.component.ts
@@ -15,6 +15,9 @@ export class FlightSearchComponent implements OnInit, OnDestroy {
   from = 'Hamburg';
   to = 'Graz';
 
+  minLength = 3;
+  maxLength = 15;
+
   flights: Flight[] = [];
   flights$: Observable<Flight[]> | undefined;
   flightsSubscription: Subscription | undefined;
