d924cd87ede9424ea45d72fed8339f4d54df5bda
Alex Thalhammer <alex@angular-software.at>
2023-06-28 13:37:48 +0200

    Standalone component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FlightBookingModule } from './flight-booking/flight-booking.module';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, FlightBookingModule],
+  imports: [BrowserModule, HttpClientModule, FlightSearchComponent],
   declarations: [AppComponent, SidebarComponent, NavbarComponent],
   bootstrap: [AppComponent]
 })
--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,12 +1,15 @@
 import { Component, DestroyRef, inject, OnDestroy } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
-import { Flight } from '../../entities/flight';
+import { Flight } from '../entities/flight';
 import { FlightService } from './flight.service';
 import { Observable, Observer, pipe, Subject, Subscription } from 'rxjs';
 import { share, takeUntil } from 'rxjs/operators';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
+  standalone: true,
+  imports: [SharedModule],
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css']
